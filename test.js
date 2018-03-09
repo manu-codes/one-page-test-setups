163238db8b9a065a2c81effddcdcd444008fcba2
Manu Krishnan <manu.krishnan@tarams.com>
2018-03-09 18:06:46 +0530

    redux test project

---
+++ test.js
@@ -0,0 +1,47 @@
+
+// import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+console.log('\n\n\n***************************START***************************\n\n')
+const { createStore, combineReducers, applyMiddleware } = require ('redux')
+
+const aReducer = (state, action) => {
+    switch (action.type) {
+        case 'A':
+            {
+                return { ...state };
+            }
+        default: return state;
+    }
+    return state;
+}
+
+const bReducer = (state, action) => {
+    switch (action.type) {
+        case 'B':
+            {
+                return { ...state };
+            }
+        default: return state;
+    }
+    return state;
+}
+
+const configureStore = (initialState) => {
+
+   
+    let rootReducer = combineReducers({ a:aReducer, b:bReducer });
+    console.log('configureStore', initialState);
+    const str = createStore(aReducer, initialState);
+    return str;
+};
+
+
+const store = configureStore({});
+
+console.log('store',store);
+
+store.subscribe(() => {
+
+    console.log(store.getState());
+
+});
